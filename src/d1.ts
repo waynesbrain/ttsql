7899ebc751acfbebdc788b73f2fec09f567fb18d
waynesbrain <175370544+waynesbrain@users.noreply.github.com>
2025-05-04 17:00:23 -0400

    Adds a prepared query cache to DatabaseD1.

--- src/d1.ts
+++ src/d1.ts
@@ -1,4 +1,8 @@
-import type { D1Database, D1ExecResult } from "@cloudflare/workers-types";
+import type {
+  D1Database,
+  D1ExecResult,
+  D1PreparedStatement,
+} from "@cloudflare/workers-types";
 // Local
 import { sql } from "./builder";
 import {
@@ -9,18 +13,34 @@ import {
 } from "./db";
 import type { Sql } from "./types";
 
+/**
+ * Cache compatible with `Map<string, D1PreparedStatement>` and
+ * [quick-lru](https://www.npmjs.com/package/quick-lru),
+ * for caching prepared statements by query.
+ */
+export interface DatabaseD1QueryCache {
+  get(query: string): D1PreparedStatement;
+  set(query: string, statement: D1PreparedStatement): any;
+}
+// CONSIDER: Add has(query: string):boolean; to DatabaseD1QueryCache if needed.
+
 export interface DatabaseD1Config<DB, A = unknown>
   extends SqlDatabaseConfig<DB, A> {
+  /** D1 Worker Database binding */
   binding: D1Database;
+  /** Cache for re-using prepared statements. Use `quick-lru` on npm. */
+  queryCache?: DatabaseD1QueryCache;
 }
 
 export class DatabaseD1<DB, A = unknown> extends SqlDatabase<DB, A> {
   protected db: D1Database;
+  protected queryCache?: DatabaseD1QueryCache;
 
   constructor(config: DatabaseD1Config<DB, A>) {
     super(config);
-    const { binding: db } = config;
+    const { binding: db, queryCache } = config;
     this.db = db;
+    this.queryCache = queryCache;
   }
   /**
    * Runs SQL with no parameters.
@@ -49,13 +69,29 @@ export class DatabaseD1<DB, A = unknown> extends SqlDatabase<DB, A> {
       throw new SqlError(err, cmd);
     }
   }
-  /** Runs the given Sql command as required by the base class. */
-  protected async run(cmd: Sql): Promise<SqlResult> {
+
+  #prepare(cmd: Sql) {
     const { query, values } = cmd;
-    let prepared = this.db.prepare(query);
+    const { db, queryCache } = this;
+    let prepared: D1PreparedStatement;
+    if (!queryCache) {
+      prepared = db.prepare(query);
+    } else {
+      prepared = queryCache.get(query);
+      if (!prepared) {
+        prepared = db.prepare(query);
+      }
+      queryCache.set(query, prepared);
+    }
     if (values.length > 0) {
       prepared = prepared.bind(...values);
     }
+    return { query, prepared, values };
+  }
+
+  /** Runs the given Sql command as required by the base class. */
+  protected async run(cmd: Sql): Promise<SqlResult> {
+    const { prepared } = this.#prepare(cmd);
     try {
       const result = await prepared.run();
       return result;
@@ -65,15 +101,7 @@ export class DatabaseD1<DB, A = unknown> extends SqlDatabase<DB, A> {
   }
 
   protected async runBatch(cmds: Sql[]): Promise<SqlResult[]> {
-    const db = this.db;
-    const statements = cmds.map((cmd) => {
-      const { query, values } = cmd;
-      let prepared = db.prepare(query);
-      if (values.length > 0) {
-        prepared = prepared.bind(...values);
-      }
-      return prepared;
-    });
+    const statements = cmds.map((cmd) => this.#prepare(cmd).prepared);
     try {
       const result = await this.db.batch(statements);
       return result;
